bb6aa76fa7728846c789eafbed46cb6d7f288f63
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-27 08:23:18 +0100

    Set up structure of the app. Start implementing beautiful dnd.

---
+++ src/initial-data.js
@@ -0,0 +1,18 @@
+const initialData = {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'Take out garbage' },
+    'task-2': { id: 'task-2', content: 'Clean Dishes' },
+    'task-3': { id: 'task-3', content: 'Buy Milk' },
+    'task-4': { id: 'task-4', content: 'Go to post office' },
+    'task-5': { id: 'task-5', content: 'Clean bathroom' }
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+    }
+  },
+  columnOrder: ['column-1']
+}
+export default initialData
